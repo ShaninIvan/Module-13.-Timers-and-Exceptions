9c9b8cae48371158150e19d8d671f61dec7afc2c
ShaninIvan <shaninivan1993@gmail.com>
2020-09-27 18:00:17 +0300

    add localStorage for settings

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
+const settings = window.localStorage;
+const inpRadio = document.querySelectorAll('input[type="radio"');
+
 window.onload = function()
 {   
+    [...inpRadio].forEach(el =>{
+        const key = el.name+el.value;
+        el.checked = (settings.getItem(key) == "false") ? false : true;
+    });
+    
+
     // TODO дописать элементы в init как тут, так и в tikTakBoom.js
     tikTakBoom.init(
         tasks,
@@ -13,9 +22,23 @@ window.onload = function()
         document.querySelector('#answer5'),
     )
     document.querySelector('.screen-start').classList.add('screen-start__show');
-    // TODO перед стартом игры проверять JSON-файл на корректность
-    document.querySelector('#startGame').addEventListener('click', () =>{
-        document.querySelector('.screen-start').classList.remove('screen-start__show');    
-        tikTakBoom.run();
+ 
+};
+
+// TODO перед стартом игры проверять JSON-файл на корректность
+document.querySelector('#startGame').addEventListener('click', () =>{
+    document.querySelector('.screen-start').classList.remove('screen-start__show');    
+    tikTakBoom.run();
+});
+
+window.addEventListener('unload', () =>{
+    settings.clear;
+    [...inpRadio].forEach(el =>{
+        const key = el.name+el.value;
+        settings.setItem(key, el.checked);
     });
-}
+})
+
+document.querySelector('#buttonReplay').addEventListener('click', () =>{
+    window.location.reload();
+});
