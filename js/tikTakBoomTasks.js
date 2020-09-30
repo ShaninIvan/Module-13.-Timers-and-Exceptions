c5ec40c90964d6831aac34fcd4f928426dcf71e4
ShaninIvan <shaninivan1993@gmail.com>
2020-09-30 17:30:12 +0300

    add new tasks

--- js/tikTakBoomTasks.js
+++ js/tikTakBoomTasks.js
@@ -25,19 +25,61 @@ const tasks = `
             "answer5": { "result": false, "value": "Тангенс" }
         },
         {
-            "question": "Столица Италии?",
+            "question": "Как называется столица Италии?",
             "answer1": { "result": true, "value": "Рим" },
             "answer2": { "result": false, "value": "Ватикан" },
             "answer3": { "result": false, "value": "Афины" },
             "answer4": { "result": false, "value": "Мадрид" },
             "answer5": { "result": false, "value": "Венеция" }
+        },
+        {
+            "question": "К какому типу животных относятся морские звезды?",
+            "answer1": { "result": true, "value": "Иглокожие" },
+            "answer2": { "result": false, "value": "Членистоногие" },
+            "answer3": { "result": false, "value": "Моллюски" },
+            "answer4": { "result": false, "value": "Кольчатые черви" },
+            "answer5": { "result": false, "value": "Хордовые" }
+        },
+        {
+            "question": "Где состоялся первый чемпионат мира по футболу",
+            "answer1": { "result": true, "value": "Уругвай" },
+            "answer2": { "result": false, "value": "Бразилия" },
+            "answer3": { "result": false, "value": "Франция" },
+            "answer4": { "result": false, "value": "Россия" },
+            "answer5": { "result": false, "value": "Канада" }
+        },
+        {
+            "question": "Какое море в Мировом Океане является самым соленым?",
+            "answer1": { "result": true, "value": "Красное море" },
+            "answer2": { "result": false, "value": "Мертвое море" },
+            "answer3": { "result": false, "value": "Азовское море" },
+            "answer4": { "result": false, "value": "Море Коро" },
+            "answer5": { "result": false, "value": "Японское море" }
+        },
+        {
+            "question": "Какой материк пересекается всеми меридианами?",
+            "answer1": { "result": true, "value": "Антарктида" },
+            "answer2": { "result": false, "value": "Евразия" },
+            "answer3": { "result": false, "value": "Австралия" },
+            "answer4": { "result": false, "value": "Северная Америка" },
+            "answer5": { "result": false, "value": "Африка" }
+        },
+        {
+            "question": "Какова длина экватора Земли?",
+            "answer1": { "result": true, "value": "40 075 км" },
+            "answer2": { "result": false, "value": "15 372 км" },
+            "answer3": { "result": false, "value": "38 996 км" },
+            "answer4": { "result": false, "value": "22 654 км" },
+            "answer5": { "result": false, "value": "57 112 км" }
         }
     ]
 `;
 
-// "question": "",
-// "answer1": { "result": true, "value": "" },
-// "answer2": { "result": false, "value": "" },
-// "answer3": { "result": false, "value": "" },
-// "answer4": { "result": false, "value": "" },
-// "answer5": { "result": false, "value": "" }
+// {
+//     "question": "",
+//     "answer1": { "result": true, "value": "" },
+//     "answer2": { "result": false, "value": "" },
+//     "answer3": { "result": false, "value": "" },
+//     "answer4": { "result": false, "value": "" },
+//     "answer5": { "result": false, "value": "" }
+// },
